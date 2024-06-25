81ad911a21a0abc590dcd73201638f0a41d86356
Bethwel123 <bethwellangat654@gmail.com>
2024-06-25 03:28:29 +0300

    Adds arithmetic operators to index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+function multiply(num1, num2) {
+    return multiply;
+}
+
+let random = Math.floor(Math.random() * 1000000) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+function mod(num3, num4) {
+    return mod;
+}
+
+let set = new Set([5, 10, 15, 20]);
+let max = Math.max(...set);
+console.log(max)
+    
